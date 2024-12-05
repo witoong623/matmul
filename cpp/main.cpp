3e181ecafab2ce4e80ab8d3c17e766213a9acfaa
Witoon Wiphusitphunpol <tenshi623@gmail.com>
2024-12-05 10:35:19 +0700

    can load array

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <string>
 
+
+constexpr int N = 1024;
+
+void load_float32_array(std::string filepath, float* array, int size) {
+  FILE* file = fopen(filepath.c_str(), "rb");
+  if (file == nullptr) {
+    std::cerr << "Error: cannot open file " << filepath << std::endl;
+    return;
+  }
+  fread(array, sizeof(float), size, file);
+  fclose(file);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  float *A = new float[N * N];
+  float *B = new float[N * N];
+  float *C = new float[N * N];
+
+  load_float32_array("../A_1024.mat", A, N * N);
+  load_float32_array("../A_1024.mat", B, N * N);
+  load_float32_array("../A_1024.mat", C, N * N);
+
+  return 0;
 }
